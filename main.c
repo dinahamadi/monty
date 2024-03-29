9503019dfa02006241d7c63b0dcc13a68f4b1875
dina <dinahamadi.20@gmail.com>
2024-03-29 00:25:50 +0000

    starting monty

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+* main -main function
+* @argc: number of arguments
+* @argv: arguments
+* Return: exit value
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+
+}
